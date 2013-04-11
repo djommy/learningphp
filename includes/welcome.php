8ac72848851a3d4f363f752f8f55c019c5493ffa
unknown <nikoladzonivojvodic@gmail.com>
2013-04-12 00:45:44 +0200

    Remove rubbish from code

--- includes/welcome.php
+++ includes/welcome.php
@@ -1,4 +1,4 @@
 
  <?php 
- $welcomeMessage = "Hello and welcome to my website, hope you like useless infomation you will find here. Have a nice " . date(l) . "!";  
+ $welcomeMessage = "Hello and welcome to my website, hope you like useless infomation you will find here. Have a nice day!";  
  ?>
